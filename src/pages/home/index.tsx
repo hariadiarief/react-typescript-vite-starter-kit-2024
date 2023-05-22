a7b2c9643fc3c0a5fdc03e430305eda3f0637ce7
hariadiarief <adi.arief77@gmail.com>
2023-05-22 20:41:00 +0700

    update: fetch data

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,87 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import { Button, Spin } from 'antd'
+
+type IpokemonList = {
+    isLoading: boolean
+
+    isLoadingMore: boolean
+    isHasMore: boolean
+    offset: number
+    limit: number
+
+    items: Ipokemon[]
+}
+type Ipokemon = {
+    name: string
+    url: string
+}
+
 export default function Home() {
-    return <div className='container home'>Home Page</div>
+    const [pokemonList, setPokemonList] = useState<IpokemonList>({
+        isLoading: false,
+        isLoadingMore: false,
+        isHasMore: false,
+        offset: 0,
+        limit: 20,
+        items: [],
+    })
+
+    const fethPokemon = () => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${pokemonList.limit}&offset=${pokemonList.offset}}`).then((response) =>
+            setPokemonList({
+                ...pokemonList,
+                isHasMore: !!response.data.next,
+                items: pokemonList.items.concat(response.data.results),
+                offset: pokemonList.offset + pokemonList.limit,
+                isLoadingMore: false,
+            })
+        )
+    }
+    useEffect(fethPokemon, [])
+
+    const loadMore = () => {
+        setPokemonList({
+            ...pokemonList,
+            isLoadingMore: true,
+        })
+        fethPokemon()
+    }
+
+    return (
+        <div className='container flex flex-col justify-center pb-12'>
+            <div className='home__grid container'>
+                {pokemonList.items.length === 0 || pokemonList.isLoading
+                    ? renderLoader()
+                    : pokemonList.items.map((pokemon, index) => {
+                          return (
+                              <Link className='home__grid__item' to={`/pokemon/${pokemon.name}`} key={index}>
+                                  <img
+                                      alt={pokemon.name}
+                                      className='home__grid__item__image'
+                                      src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`}
+                                      onError={({ currentTarget }) => {
+                                          currentTarget.src = require('assets/broken.png')
+                                      }}
+                                  />
+                                  <span>{pokemon.name}</span>
+                              </Link>
+                          )
+                      })}
+                <>{pokemonList.items.length > 0 && pokemonList.isLoadingMore ? renderLoader() : null}</>
+            </div>
+            <Button type='primary' onClick={loadMore}>
+                Load More
+            </Button>
+        </div>
+    )
+
+    function renderLoader() {
+        return Array.apply(null, Array(6)).map((_, index) => (
+            <div className='home__grid__item py-12' key={index}>
+                <Spin size='large' />
+            </div>
+        ))
+    }
 }
