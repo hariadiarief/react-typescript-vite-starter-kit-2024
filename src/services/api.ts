5b2e0edf087dcf60f7a744a6f5b1cb5495a86280
Masagus Hariadi Arief <adi.arief77@gmail.com>
2023-06-24 19:50:09 +0700

    add: add axios interceptors response

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+import { toast } from 'react-toastify'
 import { enableMockAdapter } from './mockAdapter'
 
 export const axiosInstance = axios.create({
@@ -6,3 +7,22 @@ export const axiosInstance = axios.create({
 })
 
 enableMockAdapter({ isEnabled: false })
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+        console.log('Logging the error', error)
+        console.log('Logging message', error?.message)
+        toast.error(error?.message || 'An unexpected error occured!', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            // theme: 'dark',
+        })
+        return Promise.reject(error)
+    }
+)
