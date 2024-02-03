9d6a6ce0e827e2295970c1aa3ab8ec12bf271bf9
hariadiarief <adi.arief77@gmail.com>
2024-02-03 18:54:01 +0700

    init: vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
-import { ConfigProvider, theme } from 'antd'
-import { RouterProvider } from 'react-router-dom'
-import { publicRoutes } from 'routes'
-import { AppProvider } from './contexts/appContext'
-import { useAppContext } from 'contexts/appContext'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
-    return (
-        <AppProvider>
-            <Main />
-        </AppProvider>
-    )
-}
+function App() {
+  const [count, setCount] = useState(0)
 
-const Main = () => {
-    const { isDark } = useAppContext()
-    const { defaultAlgorithm, darkAlgorithm } = theme
-
-    return (
-        <ConfigProvider
-            theme={{
-                algorithm: isDark ? darkAlgorithm : defaultAlgorithm,
-            }}
-        >
-            <RouterProvider router={publicRoutes} />
-        </ConfigProvider>
-    )
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
 export default App
